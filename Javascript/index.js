9d8787e85f01f7cec66393fc201355b0c3c66af8
Maria Vasilenko <snysic1987@gmail.com>
2024-10-20 15:51:53 +0200

    add a funcion to remove duplicates

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,11 +3,23 @@ funcion getSentence(sentence) {
 }
 
 
-funcion searchVocals(sentence)
+funcion searchVocals(sentence){
 const vocals = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 let foundVocals = [];
 
-funcion deleteDuplicity(list)
+for (let char of sentence) {
+    if (vocals.includes(char)) {
+        foundVocals.push(char.toLowerCase());
+    }
+}
+return foundVocals;
+}
+
+function deleteDuplicity(list) {
+    return [...new Set(list)]; 
+}
+
+
 
 
 funcion getVocals(list)
