523b5f61bb868c6ca6dc37b364ab76b27f4ed317
Maria Vasilenko <snysic1987@gmail.com>
2024-10-20 15:17:31 +0200

    finding and returning vowels

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,10 +1,11 @@
-
-
-funcion getSentence(sentence)
+funcion getSentence(sentence) {
+    return sentence;
+}
 
 
 funcion searchVocals(sentence)
-
+const vocals = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+let foundVocals = [];
 
 funcion deleteDuplicity(list)
 
